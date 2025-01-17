13dacbe0e89a658cab39ea3667a9cc44b720cff4
Hein <minhein301005@gmail.com>
2025-01-17 12:44:58 +0700

    Program ( Movie Manager )

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using MovieManager.Framework;
+
+namespace MovieManager
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("------- MOVIE MANAGER -------");
+
+            while (true)
+            {
+                Console.Write(">>> ");
+                var route = Console.ReadLine();
+                Router.Forward(route);
+            }
+        }
+    }
+}
