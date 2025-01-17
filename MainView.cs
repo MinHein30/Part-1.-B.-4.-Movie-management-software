429089bb72801f58fadf5d922c75317314aa1527
Hein <minhein301005@gmail.com>
2025-01-17 12:42:08 +0700

    Views

---
+++ MainView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieManager.Views
+{
+    internal class MainView
+    {
+        public void HelpView()
+        {
+            Console.WriteLine("\n--------- HELP ---------");
+            Console.WriteLine(" List - Show the list of movies");
+            Console.WriteLine(" Add - Add a new movie");
+            Console.WriteLine(" Remove - Remove a movie by ID");
+            Console.WriteLine(" Clear - Clear the console");
+            Console.WriteLine(" Help - Show this help");
+            Console.WriteLine("------------------------");
+        }
+    }
+}
---
+++ MovieView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MovieManager.Models;
+
+namespace MovieManager.Views
+{
+    internal class MovieView
+    {
+        public void ListView(Movie[] model)
+        {
+            if (model == null || model.Length == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No movies to display.");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                foreach (var movie in model)
+                {
+                    Console.WriteLine($"Id          : {movie.Id}");
+                    Console.WriteLine($"Name        : {movie.Name}");
+                    Console.WriteLine($"Length      : {movie.Length} minutes");
+                    Console.WriteLine($"Year        : {movie.Year}");
+                    Console.WriteLine($"Description : {movie.Description}");
+                    Console.WriteLine("---------------------------------------------");
+                }
+                Console.ResetColor();
+            }
+        }
+    }
+}
