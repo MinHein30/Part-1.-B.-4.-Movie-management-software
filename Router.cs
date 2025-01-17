ee8260141c7830208d105a680a7af5c29a46da23
Hein <minhein301005@gmail.com>
2025-01-17 12:43:34 +0700

    Framework ( Router )

---
+++ Router.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieManager.Controllers;
+
+namespace MovieManager.Framework
+{
+    internal static class Router
+    {
+        private static readonly MovieController MovieController = new();
+        private static readonly MainController MainController = new();
+
+        public static void Forward(string query)
+        {
+            var route = query.Trim().ToLower();
+
+            switch (route)
+            {
+                case "help":
+                    MainController.Help();
+                    break;
+                case "clear":
+                    MainController.Clear();
+                    break;
+                case "list":
+                    MovieController.List();
+                    break;
+                case "add":
+                    MovieController.Add();
+                    break;
+                case "remove":
+                    MovieController.Remove();
+                    break;
+                default:
+                    Console.WriteLine("Unknown command. Type 'help' for a list of commands.");
+                    break;
+            }
+        }
+    }
+}
